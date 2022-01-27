f4138fa80da0dbc70773e00b9abc5f88993aaeff
fernandopaz1 <ferpaz9292@gmail.com>
2022-01-26 23:21:53 -0300

    explicacion orden de midlewares

--- app.js
+++ app.js
@@ -19,12 +19,28 @@ app.use((req, res, next)=>{
     next(); // llama al siguiente midleware4
 })
 
+// si agrego un path lo tengo que poner antes
+// esto es porque el primer path que hace match se ejecuta
+// si ejecuto este y envio el html, el siguiente midleware nose ejecuta
+// Pero si pongo un url mas generico como '/' al principio, este siempre
+// machea y nunca lo van a hacer los demas 
+// los midlewares que tienen que ser aplicados a todos los request va 
+// antes que todos los que envian una respuesta
+app.use('/about',(req, res, next)=>{
+    console.log("in the second midleware");
+    // res.send() por default setea un header con el tipo correcto
+    res.send('<h1>Hola este es el about</h1>'); 
+})
 
+
+// app.use tiene varios overloads para poder usarla de dferentes formas
+// dependiendo de los parametros que usamos
 app.use((req, res, next)=>{
     console.log("in the second midleware");
     // res.send() por default setea un header con el tipo correcto
     res.send('<h1>Hola desde express</h1>'); 
 })
+
 
 // El valor que retorna la función express resulta ser también
 // un requestHandler valido, por lo que lo podemos pasar como
